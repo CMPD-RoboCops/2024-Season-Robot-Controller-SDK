c622648646505813e39d054f009bc1ddbd7f2e9e
Alex <2kidactive@gmail.com>
2024-12-09 18:24:48 -0500

    Implement AprilTag Localization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
@@ -3,11 +3,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.util.List;
 
@@ -23,7 +28,11 @@ public class ComputerVision extends OpMode {
     public void init() {
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         openCVProcessor = new OpenCVProcessor();
-        aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        Position cameraPos = new Position(DistanceUnit.INCH, 0, 0, 0, 0);
+        YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES, 0, -90, 0, 0);
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPos, cameraOrientation)
+                .build();
         visionPortal = VisionPortal.easyCreateWithDefaults(webcamName, openCVProcessor, aprilTagProcessor);
     }
 
@@ -32,10 +41,25 @@ public class ComputerVision extends OpMode {
         List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
         StringBuilder idsFound = new StringBuilder();
         for (AprilTagDetection detection : currentDetections) {
-            idsFound.append(detection.id);
-            idsFound.append(' ');
+            double myX = detection.robotPose.getPosition().x;
+            double myY = detection.robotPose.getPosition().y;
+            double myZ = detection.robotPose.getPosition().z;
+
+            double myPitch = detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES);
+            double myRoll = detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES);
+            double myYaw = detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES);
+
+            telemetry.addData("X", myX);
+            telemetry.addData("Y", myY);
+            telemetry.addData("Z", myZ);
+
+            telemetry.addData("Pitch", myPitch);
+            telemetry.addData("Roll", myRoll);
+            telemetry.addData("Yaw", myYaw);
+
+            telemetry.update();
+            break;
         }
-        telemetry.addData("April Tags", idsFound);
     }
 
     @Override
